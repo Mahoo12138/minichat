7ec79487f239836c2529eb91dc2cc41d929f42f6
Mahoo12138 <mahoo12138@qq.com>
2024-09-25 14:53:52 +0800

    feat: 针对开发模式下自动读取默认配置文件

--- config/config.go
+++ config/config.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 type Config struct {
@@ -16,15 +18,7 @@ var GlobalConfig *Config
 
 func ParseConfig(filename string) *Config {
 
-	// 获取当前可执行文件的完整路径
-	executable, err := os.Executable()
-	if err != nil {
-		log.Fatalf("\n\nUnable to get executable path: %+v\n\n", err)
-	}
-
-	// 如使用IDE调试，请改为本地路径
-	dir := filepath.Dir(executable)
-	configPath := filepath.Join(dir, filename)
+	configPath := getConfigPath(filename)
 
 	data, err := os.ReadFile(configPath)
 	if err != nil {
@@ -38,4 +32,33 @@ func ParseConfig(filename string) *Config {
 	}
 	GlobalConfig = cfg
 	return cfg
+}
+
+
+func getConfigPath(filename string) string {
+	// 通过环境变量判断是否是开发环境
+	executable, err := os.Executable()
+	if err != nil {
+		log.Fatalf("\n\nUnable to get executable path: %+v\n\n", err)
+	}
+	tmpDir := os.TempDir()
+	// 临时目录下，为开发调试环境
+	if strings.HasPrefix(executable, tmpDir) {
+		_, mainFilePath, _, ok := runtime.Caller(0)
+
+		if !ok {
+			log.Fatalf("Unable to determine caller information")
+		}
+
+		// 定位 main.go 文件所在的目录, 即项目根目录
+		projectRoot := filepath.Join(filepath.Dir(mainFilePath), "../")
+
+		log.Println("root: ", mainFilePath, projectRoot)
+
+		return filepath.Join(projectRoot, filename)
+	}
+
+
+	dir := filepath.Dir(executable)
+	return filepath.Join(dir, filename)
 }
